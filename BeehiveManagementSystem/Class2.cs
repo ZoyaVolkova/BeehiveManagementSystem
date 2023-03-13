533e1b86a7b21a188538e8d3dec1ee7947a4af49
ZoyaVolkova <zoearkhipova@gmail.com>
2023-03-13 14:01:37 +0300

    Добавьте файлы проекта.

---
+++ BeehiveManagementSystem/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem
+{
+    internal class Bee
+    {
+        public string Job {get; private set;}
+        public Bee(string job) {
+            Job = job;
+        }
+    }
+}
